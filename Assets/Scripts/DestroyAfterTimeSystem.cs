6c6442c85cbf3fe437b7350b6177811638441469
Antonio Ruggiero <ant.ruggiero@gmail.com>
2020-12-04 17:16:16 +0100

    initial commit

---
+++ Assets/Scripts/DestroyAfterTimeSystem.cs
@@ -0,0 +1,52 @@
+﻿using Unity.Entities;
+
+public class DestroyAfterTimeSystem : SystemBase
+{
+    
+    private EndSimulationEntityCommandBufferSystem _commandBufferSystem;
+    
+    
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        // Find the ECB system once and store it for later usage
+        _commandBufferSystem = World
+            .GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    protected override void OnUpdate()
+    {
+        // Assign values to local variables captured in your job here, so that it has
+        // everything it needs to do its work when it runs later.
+        // For example,
+        //     float deltaTime = Time.DeltaTime;
+
+        // This declares a new kind of job, which is a unit of work to do.
+        // The job is declared as an Entities.ForEach with the target components as parameters,
+        // meaning it will process all entities in the world that have both
+        // Translation and Rotation components. Change it to process the component
+        // types you want.
+
+        var commandBuffer = _commandBufferSystem.CreateCommandBuffer();
+        
+        var actualTime = Time.ElapsedTime;
+        
+        Entities.ForEach((in Entity entity,in DestroyAfterTime destroyAfterTime) => {
+            // Implement the work to perform for each entity here.
+            // You should only access data that is local or that is a
+            // field on this job. Note that the 'rotation' parameter is
+            // marked as 'in', which means it cannot be modified,
+            // but allows this job to run in parallel with other jobs
+            // that want to read Rotation component data.
+            // For example,
+            //     translation.Value += math.mul(rotation.Value, new float3(0, 0, 1)) * deltaTime;
+            
+            if (actualTime - destroyAfterTime.BirthTime < destroyAfterTime.CountDown) return;
+            
+            commandBuffer.DestroyEntity(entity);
+            
+        }).Schedule();
+        
+        _commandBufferSystem.AddJobHandleForProducer(Dependency);
+    }
+}
---
+++ Assets/Scripts/DestroyOnCollisionSystem.cs
@@ -0,0 +1,66 @@
+﻿using Unity.Entities;
+using Unity.Physics;
+using Unity.Physics.Systems;
+
+public class DestroyOnCollisionSystem : SystemBase
+{
+    
+    private EndSimulationEntityCommandBufferSystem _commandBufferSystem;
+    
+    
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        // Find the ECB system once and store it for later usage
+        _commandBufferSystem = World
+            .GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+
+        _physicsWorld = World.GetOrCreateSystem<BuildPhysicsWorld>();
+        _stepPhysicsWorld = World.GetOrCreateSystem<StepPhysicsWorld>();
+    }
+
+    public struct DestroyOnCollision : ITriggerEventsJob
+    {
+        public EntityCommandBuffer _commandBuffer;
+        
+        
+        public void Execute(TriggerEvent triggerEvent)
+        {
+            _commandBuffer.DestroyEntity(triggerEvent.EntityA);
+            _commandBuffer.DestroyEntity(triggerEvent.EntityB);
+        }
+    }
+
+    private BuildPhysicsWorld _physicsWorld;
+    private StepPhysicsWorld _stepPhysicsWorld;
+    
+    
+    
+    protected override void OnUpdate()
+    {
+        // Assign values to local variables captured in your job here, so that it has
+        // everything it needs to do its work when it runs later.
+        // For example,
+        //     float deltaTime = Time.DeltaTime;
+
+        // This declares a new kind of job, which is a unit of work to do.
+        // The job is declared as an Entities.ForEach with the target components as parameters,
+        // meaning it will process all entities in the world that have both
+        // Translation and Rotation components. Change it to process the component
+        // types you want.
+
+
+        var commandBuffer = _commandBufferSystem.CreateCommandBuffer();
+
+        var a = new DestroyOnCollision()
+        {
+            _commandBuffer = commandBuffer
+        };
+
+
+        var handle=a.Schedule(_stepPhysicsWorld.Simulation, ref _physicsWorld.PhysicsWorld, Dependency);
+        
+        _commandBufferSystem.AddJobHandleForProducer(handle);
+        
+    }
+}
